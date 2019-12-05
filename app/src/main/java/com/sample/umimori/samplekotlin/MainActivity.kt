db731487d11dcb116fa5e56343db515be5aa7e5b
Masumi.M <masumi.lacrosse@live.jp>
2019-12-05 21:27:57 +0900

    Initial Commit

---
+++ app/src/main/java/com/sample/umimori/samplekotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sample.umimori.samplekotlin
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        // Change the text of textView when the button pressed
+        val textView = findViewById<TextView>(R.id.message) as TextView
+        val button = findViewById<Button>(R.id.button) as Button
+
+        textView.text = "Before"
+        button.setOnClickListener{ v->
+            textView.text = "After"
+        }
+
+
+    }
+}
